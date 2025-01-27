63425d46cdd6c3903e1f430554b2c45b0e9d0d91
dlcodb94 <109951560+dlcodb94@users.noreply.github.com>
2025-01-27 21:27:10 +0900

    프로젝트 구조 설정

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const sampleRoutes = require('./routes/sampleRoutes.js');
 const app = express();
@@ -8,3 +9,22 @@ app.use(express.json());
 app.use('/api/sample', sampleRoutes);
 
 module.exports = app;
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const recoveryRoutes = require('./routes/recovery');
+
+const app = express();
+const port = 3000;
+
+// Middleware 설정
+app.use(bodyParser.json());
+
+// 라우트 설정
+app.use('/recovery', recoveryRoutes);
+
+// 서버 실행
+app.listen(port, () => {
+  console.log(`서버 실행 중: http://localhost:${port}`);
+});

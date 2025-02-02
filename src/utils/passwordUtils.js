377e280374b2a50f02f0a1b037f20c3b38ca1159
SIGAE-DM <ehdals5387@naver.com>
2025-02-03 01:32:51 +0900

    비밀번호 해싱 설정

--- src/utils/passwordUtils.js
+++ src/utils/passwordUtils.js
@@ -1,13 +1,10 @@
-// 해싱 방법을 어떻게 할까요?
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 async function hashPassword(password) {
-  return await bcrypt.hash(password);
-  //const salt = await bcrypt.genSalt(10);
-  //return await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
 }
 
-// 비밀번호 비교 함수
 async function comparePassword(plainPassword, hashedPassword) {
   return bcrypt.compare(plainPassword, hashedPassword);
 }

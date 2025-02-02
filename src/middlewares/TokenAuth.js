f51efea3d09c49cb5219223a0d87cfb9d454ca46
SIGAE-DM <ehdals5387@naver.com>
2025-02-03 01:39:39 +0900

    토큰 생성 파일 병합

--- src/middlewares/TokenAuth.js
+++ src/middlewares/TokenAuth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const User = require('../models/User');
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -19,4 +20,40 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+const resetTokenMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .json({ message: '비밀번호 재설정 토큰이 필요합니다.' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+
+    if (!decoded.email) {
+      return res
+        .status(401)
+        .json({ message: '유효하지 않은 비밀번호 재설정 토큰입니다.' });
+    }
+
+    const user = await User.findOne({ email: decoded.email });
+
+    if (!user) {
+      return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+    }
+
+    req.user = user;
+    return next();
+  } catch (error) {
+    console.error('비밀번호 재설정 토큰 검증 오류:', error);
+    return res
+      .status(401)
+      .json({ message: '유효하지 않은 비밀번호 재설정 토큰입니다.' });
+  }
+};
+
+module.exports = { authMiddleware, resetTokenMiddleware };

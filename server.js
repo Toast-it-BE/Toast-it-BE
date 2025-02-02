33ec7cf752ad3041b85da97ff0820b5e9e3dbb0c
SIGAE-DM <ehdals5387@naver.com>
2025-02-03 01:42:30 +0900

    불필요 파일 삭제

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app.js');
-const config = require('./src/config');
-
-const PORT = config.port || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});

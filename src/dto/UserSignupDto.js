ed319535bcc4a03b22732ff9f697ac929b7cfc9c
SIGAE-DM <ehdals5387@naver.com>
2025-02-03 00:05:55 +0900

    DTO 로직 수정

--- src/dto/UserSignupDto.js
+++ src/dto/UserSignupDto.js
@@ -10,20 +10,28 @@ class UserSignupDTO {
       throw new Error('이메일과 비밀번호는 필수 입력값입니다.');
     }
 
+    this.validateEmail();
+
+    this.validatePassword();
+
+    if (this.password !== this.confirm_password) {
+      throw new Error('두 비밀번호가 일치하지 않습니다.');
+    }
+  }
+
+  validateEmail() {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(this.email)) {
+    if (!this.email || !emailRegex.test(this.email)) {
       throw new Error('이메일의 형식이 올바르지 않습니다.');
     }
+  }
 
+  validatePassword() {
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     if (!passwordRegex.test(this.password)) {
       throw new Error(
         '비밀번호는 8자리 이상이어야 하며, 영어와 숫자를 반드시 포함해야 합니다.',
       );
-    }
-
-    if (this.password !== this.confirm_password) {
-      throw new Error('두 비밀번호가 일치하지 않습니다.');
     }
   }
 }

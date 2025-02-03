dab74acf92ef2b6e97ccea9636044e3d0c2d9344
SIGAE-DM <ehdals5387@naver.com>
2025-02-03 17:14:55 +0900

    cors 설정

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const passwordRoutes = require('./routes/passwordRoutes');
 const connectDB = require('./utils/db');
 const authRoutes = require('./routes/AuthRoutes');
 const categoryRoutes = require('./routes/CategoryRoutes');
 const memoRoutes = require('./routes/MemoRoutes');
+const config = require('./config');
 
 const app = express();
 app.use(bodyParser.json());
 
 // MongoDB 연결
 connectDB();
+
+// CORS 설정
+app.use(cors());
+
+app.use(
+  cors({
+    origin: 'http://localhost:8000', // 허용할 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE', // 허용할 메소드
+    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더
+  }),
+);
 
 // 미들웨어 설정
 app.use(express.json());
@@ -20,6 +34,6 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/memos', memoRoutes);
 app.use('/api/auth/password', passwordRoutes);
 
-app.listen(3000, () => {
-  console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
+app.listen(config.port, () => {
+  console.log('서버가 http://localhost:8000 에서 실행 중입니다.');
 });

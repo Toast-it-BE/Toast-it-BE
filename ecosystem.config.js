e76c5b0aa42e97f5660e1e6cbf2ba1003c990e1b
SIGAE-DM <ehdals5387@naver.com>
2025-02-10 18:24:49 +0900

    클러스터 비활성화

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'toast-it-api',
-      script: 'src/app.js', // 실행할 파일
-      instances: 'max', // 모든 CPU 코어 사용
-      exec_mode: 'cluster', // 클러스터 모드
-      watch: false, // 코드 변경 시 자동 재시작 (개발 환경에서는 true 가능)
+      script: 'src/app.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
       env: {
         NODE_ENV: 'production',
         PORT: 8000,

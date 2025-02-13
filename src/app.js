158143635becf33c1b11c87853eeb5afc2e97ca5
SIGAE-DM <ehdals5387@naver.com>
2025-02-13 18:06:44 +0900

    cors 추가

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const passwordRoutes = require('./routes/passwordRoutes');
 const connectDB = require('./utils/db');
 const authRoutes = require('./routes/AuthRoutes');
@@ -12,6 +13,19 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      'https://www.toast-it.site',
+      'https://toast-it.site',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 // 테스트 라우트
 app.get('/', (req, res) => {
